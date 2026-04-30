d62466e2ad9eec62bf33ef8812e6c6da4e19f1e4
keane <keane7a@gmail.com>
2026-04-30 20:10:47 +0100

    add test and gitlab

--- chapter-3/main.py
+++ chapter-3/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI 
+from starlette_exporter import PrometheusMiddleware, handle_metrics
 import pyjokes
 
 app = FastAPI()
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
+
 @app.get("/")
 async def root(): 
     random_joke = pyjokes.get_joke("en", "all")
